bddd1528b2559983636cd01ac678f23cd73e3435
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2024-02-05 10:50:28 +0530

    Time: 14 ms (66.07%) | Memory: 46.8 MB (72.12%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,39 +1,47 @@
+class Node{
+    int first;
+    int second;
+    int third;
+    Node(int _first, int _second, int _third){
+        this.first=_first;
+        this.second=_second;
+        this.third=_third;
+    }
+}
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        Deque<int[]> q = new ArrayDeque<>();
-        int length = 0;
-        int[] dirX = {1, -1, 0, 0}, dirY = {0, 0, 1, -1};
-
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[0].length; j++) {
-                if (mat[i][j] == 0) {
-                    q.addFirst(new int[] {i,j});
-                } else {
-                    mat[i][j] = -1;
+        int n=mat.length;
+        int m=mat[0].length;
+        int [][]vis=new int[n][m];
+        int [][]dist=new int[n][m];
+        Queue<Node> q=new LinkedList<Node>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    q.add(new Node(i,j,0));
+                    vis[i][j]=1;
                 }
             }
         }
 
-        while (!q.isEmpty()) {
-            int levelSize = q.size();
-            length++;
-            while (levelSize-- > 0) {
-                int[] pos = q.removeLast();
-                for (int i = 0; i < 4; i++) {
-                    int x = pos[0] + dirX[i];
-                    int y = pos[1] + dirY[i];
-                    if (checkBounds(mat.length, mat[0].length, x, y) && mat[x][y] < 0) {
-                        mat[x][y] = length;
-                        q.addFirst(new int[] {x,y});
-                    }
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,-1,0,1};
+
+        while(!q.isEmpty()){
+            int r=q.peek().first;
+            int c=q.peek().second;
+            int steps=q.peek().third;
+            q.remove();
+            dist[r][c]=steps;
+            for(int i=0;i<4;i++){
+                int nrow=r+drow[i];
+                int ncol=c+dcol[i];
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && vis[nrow][ncol]==0){
+                    vis[nrow][ncol]=1;
+                    q.add(new Node(nrow,ncol,steps+1));
                 }
             }
         }
-
-        return mat;
-    }
-
-    private boolean checkBounds(int r, int c, int x, int y) {
-        return x >= 0 && x < r && y >= 0 && y < c;
+        return dist;
     }
 }
