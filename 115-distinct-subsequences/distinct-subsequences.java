cde1ae5ab4296e23c5f1c3fe97b2dc4ca28d3618
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2024-04-02 10:50:44 +0530

    Time: 13 ms (77.73%) | Memory: 48.9 MB (38.31%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,27 +1,33 @@
 class Solution {
+    int fn(String s, String t, int i, int j, int[][] dp) {
+        // If we reach the end of t, it means we found a valid subsequence
+        if (j < 0) return 1;
+        // If we reach the end of s but not the end of t, return 0
+        if (i < 0) return 0;
+        // If the value is already computed, return it
+        if (dp[i][j] != -1) return dp[i][j];
+        
+        int ans = 0;
+        // If the characters match, we can either include or exclude the character
+        if (s.charAt(i) == t.charAt(j)) {
+            dp[i][j] = fn(s, t, i - 1, j - 1, dp) + fn(s, t, i - 1, j, dp);
+        } else {
+            // If the characters don't match, we can only exclude the character from s
+            dp[i][j] = fn(s, t, i - 1, j, dp);
+        }
+        // Return the computed value
+        return dp[i][j];
+    }
+
     public int numDistinct(String s, String t) {
         int n = s.length();
         int m = t.length();
-        int[][] dp = new int[n + 1][m + 1];
-        
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = 1;
+        // Create a memoization table
+        int[][] dp = new int[n][m];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
         }
-        
-        for (int i = 1; i <= m; i++) {
-            dp[0][i] = 0;
-        }
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (s.charAt(i - 1) == t.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
-        }
-
-        return dp[n][m];
+        // Start recursion from the end of both strings
+        return fn(s, t, n - 1, m - 1, dp);
     }
 }
