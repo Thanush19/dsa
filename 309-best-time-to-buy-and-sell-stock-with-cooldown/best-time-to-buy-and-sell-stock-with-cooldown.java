ab79131b725510e308be873afd943b8d1c4fc2a9
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2024-04-07 13:20:33 +0530

    Time: 1 ms (80.27%) | Memory: 41.7 MB (50.10%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -0,0 +1,25 @@
+class Solution {
+    int fn(int[] a , int idx, int flag , int[][] dp){
+        if(idx>=a.length ) return 0;
+        if(dp[idx][flag]!=-1) return dp[idx][flag];
+        int pro=0;
+        if(flag==0){
+            pro = Math.max(-a[idx]+fn(a,idx+1,1,dp),fn(a,idx+1,0,dp));
+
+        }
+        else{
+            pro = Math.max(a[idx]+fn(a,idx+2,0,dp),fn(a,idx+1,1,dp));
+        }
+        return dp[idx][flag]=pro;
+    }
+    public int maxProfit(int[] a) {
+
+        int   n = a.length;
+        if(n==1) return 0;
+        int[][]dp =new int[n+1][2];
+        for(int[]  i :dp) Arrays.fill(i,-1);
+        return fn(a,0,0,dp);
+
+        
+    }
+}
