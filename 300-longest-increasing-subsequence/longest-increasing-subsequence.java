a51e8ac975fb1240e6a60459374d2c0695875888
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2024-04-09 11:35:05 +0530

    Time: 75 ms (19.02%) | Memory: 77 MB (13.18%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,23 +1,27 @@
 class Solution {
-    int fn(int[] a, int idx , int prev, int[][] dp) {
-        if(idx==a.length) return 0;
-        if(dp[idx][prev+1]!=-1) return dp[idx][prev+1];
-        //without taking the element , we are just moving to nxt elemt , so prev will be same; , i dont add extra value in len
-        int no = 0+fn(a,idx+1,prev,dp); 
-        int yes = 0;
-        if(prev==-1 ||(prev >= 0 && a[idx] > a[prev])){
-            yes =1+ fn(a,idx+1,idx,dp);
+  
+    public int lengthOfLIS(int[] arr) {
+        int n = arr.length;
+        int dp[][]=new int[n+1][n+1];
+    
+        for(int ind = n-1; ind>=0; ind --){
+            for (int prev_index = ind-1; prev_index >=-1; prev_index --){
+                
+                int notTake = 0 + dp[ind+1][prev_index +1];
+        
+                int take = 0;
+        
+                if(prev_index == -1 || arr[ind] > arr[prev_index]){
+                    
+                    take = 1 + dp[ind+1][ind+1];
+                }
+        
+                dp[ind][prev_index+1] = Math.max(notTake,take);
+                
+            }
         }
-        dp[idx][prev+1]=Math.max(no,yes);
-        //coordinate change to +1 for prev , because it starts from -1, i cant store store -1 in dp, that's why'
-        return dp[idx][prev+1];
-
-    }
-    public int lengthOfLIS(int[] a) {
-        int n = a.length;
-        int[][] dp = new int[n][n+1];
-        for(int[] i : dp) Arrays.fill(i,-1);
-        return fn(a,0,-1,dp);
+        
+        return dp[0][0];
         
     }
 }
