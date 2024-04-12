c4408a7bc79802eca27c3c7f180ba7491bc4ded3
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2024-04-12 08:10:11 +0530

    Time: 50 ms (30.00%) | Memory: 44.1 MB (48.72%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,27 +1,39 @@
 class Solution {
   
     public int lengthOfLIS(int[] arr) {
-        int n = arr.length;
-        int dp[][]=new int[n+1][n+1];
+        // int n = arr.length;
+        // int dp[][]=new int[n+1][n+1];
     
-        for(int ind = n-1; ind>=0; ind --){
-            for (int prev_index = ind-1; prev_index >=-1; prev_index --){
+        // for(int ind = n-1; ind>=0; ind --){
+        //     for (int prev_index = ind-1; prev_index >=-1; prev_index --){
                 
-                int notTake = 0 + dp[ind+1][prev_index +1];
+        //         int notTake = 0 + dp[ind+1][prev_index +1];
         
-                int take = 0;
+        //         int take = 0;
         
-                if(prev_index == -1 || arr[ind] > arr[prev_index]){
+        //         if(prev_index == -1 || arr[ind] > arr[prev_index]){
                     
-                    take = 1 + dp[ind+1][ind+1];
-                }
+        //             take = 1 + dp[ind+1][ind+1];
+        //         }
         
-                dp[ind][prev_index+1] = Math.max(notTake,take);
+        //         dp[ind][prev_index+1] = Math.max(notTake,take);
                 
+        //     }
+        // }
+        int n = arr.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp,1);
+        for(int i = 0;i<n;i++){
+            for(int j = 0 ;j<i;j++){
+                if(arr[j]<arr[i]){
+                    dp[i]= Math.max(dp[i],1+dp[j]);
+                }
             }
         }
+        return Arrays.stream(dp).max().getAsInt();
+
         
-        return dp[0][0];
+        // return dp[0][0];
         
     }
 }
