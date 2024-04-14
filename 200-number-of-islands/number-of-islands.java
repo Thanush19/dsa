bf9e119078fe8d22cf877cedc963091056a21052
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2024-04-14 18:06:19 +0530

    Time: 8 ms (15.00%) | Memory: 52.3 MB (9.49%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,28 +1,38 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
+    class Pair{
+        int i ;
+        int j ;
+        Pair(int i , int j){
+            this.i= i;
+            this.j=j;
+        }
+    }
+    public int numIslands(char[][] a) {
+        int n = a.length;
+        int m = a[0].length;
         boolean[][] vis = new boolean[n][m];
-        Queue<int[]> q = new LinkedList<>();
-        int cnt =0;
+        Queue<Pair> q = new LinkedList<>();
         int[] dx = {-1,0,1,0};
-        int[] dy = {0,1,0,-1};
+        int [] dy = {0,1,0,-1};
+        int cnt=0;
         for(int i =0;i<n;i++){
-            for(int j =0;j<m;j++){
-                if(grid[i][j]=='1' && !vis[i][j]){
-                    q.offer(new int[]{i,j});
+            for(int j = 0 ; j<m;j++){
+                if(a[i][j]=='1' && !vis[i][j]){
+                    q.offer(new Pair(i,j));
+                    vis[i][j] = true;
                     cnt++;
-                     while(!q.isEmpty()){
-                        int[] aa = q.poll();
-                        int  ii =aa[0];
-                        int  jj =aa[1];
-                        vis[ii][jj]=true;
-                        for(int k =0;k<4;k++){
-                            int nr = ii+dx[k];
-                            int nc = jj+dy[k];
-                             if (nr >= 0 && nr < n && nc >= 0 && nc < m && !vis[nr][nc] && grid[nr][nc] == '1') {
-                                q.offer(new int[]{nr, nc});
-                                vis[nr][nc] = true;
+                    while(!q.isEmpty()){
+                        Pair p = q.poll();
+                        int r = p.i;
+                        int c = p.j;
+                        
+                        for(int k = 0; k<4;k++){
+                            int nr = dx[k]+r;
+                            int nc = dy[k]+c;
+                            if(nr>-1 && nr< n && nc>-1 && nc<m && !vis[nr][nc]&& a[nr][nc] == '1'){
+                                vis[nr][nc]=true;
+                                q.offer(new Pair(nr,nc));
+
                             }
                         }
                     }
@@ -30,5 +40,7 @@ class Solution {
             }
         }
         return cnt;
+
+        
     }
 }
