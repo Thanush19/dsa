a136e9898da9504539e70d706bbe97531b06830b
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2024-04-18 08:39:18 +0530

    Time: 5 ms (51.43%) | Memory: 45.7 MB (41.79%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -1,18 +1,27 @@
 class Solution {
-    public int islandPerimeter(int[][] grid) {
-      int output = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == 1) {
-                    output += 4;
+    public int islandPerimeter(int[][] a) {
+        int n = a.length;
+        int m = a[0].length;
+        int perimeter = 0;
 
-                    output -= (j - 1 >= 0 && grid[i][j - 1] == 1) ? 1  : 0;
-                    output -= (j + 1 != grid[i].length && grid[i][j + 1] == 1)  ? 1 : 0;
-                    output -= (i - 1 >= 0 && grid[i - 1][j] == 1 ) ? 1 : 0;
-                    output -= (i + 1 != grid.length && grid[i + 1][j] == 1) ? 1 : 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (a[i][j] == 1) {
+                    perimeter += 4; // Add 4 for each land cell
+
+                    // Check neighboring cells
+                    if (i - 1 >= 0 && a[i - 1][j] == 1) // Up
+                        perimeter -= 1;
+                    if (j + 1 < m && a[i][j + 1] == 1) // Right
+                        perimeter -= 1;
+                    if (i + 1 < n && a[i + 1][j] == 1) // Down
+                        perimeter -= 1;
+                    if (j - 1 >= 0 && a[i][j - 1] == 1) // Left
+                        perimeter -= 1;
                 }
             }
         }
-        return output;
+
+        return perimeter;
     }
 }
