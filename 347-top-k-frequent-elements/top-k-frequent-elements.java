b09f26e29bd91ce276acd16f08dfb9ee8c807705
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2024-05-11 13:13:42 +0530

    Time: 14 ms (62.86%) | Memory: 49 MB (7.73%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,22 +1,28 @@
-import java.util.*;
-
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> mp = new HashMap<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> mp.get(b) - mp.get(a));
+    class Pair{
+        int e ,f;
+        Pair(int e,int f){
+            this.e=e;
+            this.f=f;
+        }
+    }
+    public int[] topKFrequent(int[] a, int k) {
+
+        HashMap<Integer,Integer> mp = new HashMap<>();
+        for(int i : a){
+            mp.put(i,mp.getOrDefault(i,0)+1);
+        }
         int[] ans = new int[k];
-        
-        for (int num : nums) {
-            mp.put(num, mp.getOrDefault(num, 0) + 1);
+        PriorityQueue<Pair> pq  = new PriorityQueue<>((x,y)->y.f-x.f);
+        for(Map.Entry<Integer,Integer>  i : mp.entrySet()){
+            pq.offer(new Pair(i.getKey(),i.getValue()));
         }
-        
-        pq.addAll(mp.keySet());
-        
-        int idx = 0;
-        while (k-- > 0) {
-            ans[idx++] = pq.poll();
+        int i =0;
+        while(k-- >0){
+            Pair p = pq.poll();
+            ans[i++]=p.e;
         }
-        
         return ans;
+        
     }
 }
