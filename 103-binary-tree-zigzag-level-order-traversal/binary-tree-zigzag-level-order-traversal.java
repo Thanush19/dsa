16e93d463e335ac98b48935127aa8b5a2267e39a
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2024-05-13 08:28:34 +0530

    Time: 1 ms (74.99%) | Memory: 41.8 MB (90.72%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -1,35 +1,48 @@
-import java.util.*;
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
+        boolean lTr = true;
         if (root == null) return ans;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            //
+            int n = q.size();
+            List<Integer> temp =new ArrayList<>();
 
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        boolean leftToRight = true; // Flag to track the traversal direction
-
-        while (!queue.isEmpty()) {
-            int levelSize = queue.size();
-            List<Integer> levelNodes = new ArrayList<>();
-
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode node = queue.poll();
-
-                if (leftToRight) {
-                    levelNodes.add(node.val); 
-                } else {
-                    levelNodes.add(0, node.val); 
+            for(int i = 0  ; i<n;i++){
+                 TreeNode p = q.poll();
+                 
+                 if(lTr){
+                    temp.add(p.val);
                 }
-
-                if (node.left != null) queue.offer(node.left);
-                if (node.right != null) queue.offer(node.right);
+                else{
+                    temp.add(0,p.val);
+                }
+                if(p.left!=null) q.offer(p.left);
+                if(p.right!=null) q.offer(p.right);
             }
+            lTr=!lTr;
+            ans.add(temp);
 
-            ans.add(levelNodes);
-            leftToRight = !leftToRight;
         }
-
         return ans;
+
+        
     }
 }
