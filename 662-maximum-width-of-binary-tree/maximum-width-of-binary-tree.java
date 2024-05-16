e6091de7d75f502fc68a9cc9fd7d8c0d7c710062
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2024-05-16 09:36:02 +0530

    Time: 1 ms (98.36%) | Memory: 43 MB (70.45%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -1,41 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public int widthOfBinaryTree(TreeNode root) {
-        Queue<Tuple> q = new LinkedList<>();
-        q.add(new Tuple(1, 0, root)); // num, level, node
-        int prevNum = 1, prevLevel = 0;
-        int res = 0;
-
-        while(!q.isEmpty()) {
-            Tuple curr = q.remove();
-            int num = curr.num, level = curr.level;
-            TreeNode node = curr.node;
-
-            if(prevLevel < level) {
-                prevLevel = level;
-                prevNum = num;
-            }
-
-            res = Math.max(res, num - prevNum +1);
-
-            if(node.left != null) {
-                q.add(new Tuple(num*2, level+1, node.left));
-            } if(node.right != null) {
-                q.add(new Tuple(num*2+1, level+1, node.right));
-            }
-        }
-
-        return res;
-    }
-
-    class Tuple {
-        int num;
-        int level;
+    class Tuple{
+        int lvl,num;
         TreeNode node;
-
-        Tuple(int num, int level, TreeNode node) {
+        Tuple(int lvl ,int num , TreeNode node){
+            this.lvl = lvl;
             this.num = num;
-            this.level = level;
             this.node = node;
         }
+    }
+    public int widthOfBinaryTree(TreeNode root) {
+        if (root == null )return 0;
+        Queue<Tuple> q  = new LinkedList<>();
+        q.offer(new Tuple(1,1,root));     
+        int prevNum = 1 , prevLvl = 0,res=Integer.MIN_VALUE;
+        while(!q.isEmpty()){
+            int n = q.size();
+            for(int i =0;i<n;i++){
+                Tuple p = q.poll();
+                int lvl = p.lvl;
+                int num = p.num;
+                TreeNode node = p.node;
+                if(prevLvl < lvl ){
+                        prevLvl=lvl;
+                        prevNum = num;
+                }
+                if(node.left!=null) q.offer(new Tuple(lvl+1,2*num,node.left));
+                if(node.right!=null) q.offer(new Tuple(lvl+1,2*num+1,node.right));
+                res= Math.max(res,num-prevNum+1);
+
+            }
+            
+
+        }   
+        return res;
     }
 }
