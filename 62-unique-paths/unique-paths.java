6e623a4ee161c45a207804c7e4c2916138164ef3
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2024-06-10 10:49:39 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (64.84%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,17 +1,19 @@
 class Solution {
-    int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        dp[0][0] = 1;
+    int fn(int n , int m , int[][] dp){
+        if(n<0 || m<0) return 0;
+        if(n==0 && m==0) return 1;
+        if(dp[n][m]!=-1) return dp[n][m];
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i > 0)
-                    dp[i][j] += dp[i - 1][j]; // Add the value from above cell
-                if (j > 0)
-                    dp[i][j] += dp[i][j - 1]; // Add the value from left cell
-            }
+        int up = fn(n-1,m,dp);
+        int down = fn(n,m-1,dp);
+        return dp[n][m]=up+down;
+    }
+    public int uniquePaths(int n, int m) {
+        int[][] dp = new int[n][m];
+        for(int[] i : dp){
+            Arrays.fill(i,-1);
         }
-
-        return dp[m - 1][n - 1];
+        return fn(n-1,m-1,dp);
+        
     }
 }
