82b891adec8056e78ccd679a30cee709d84f262c
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2024-06-14 19:49:37 +0530

    Time: 2 ms (41.89%) | Memory: 43.7 MB (91.89%) - LeetSync

---
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public Node connect(Node root) {
+        //check for null input 
+        if(root == null)return root;
+        //make a queue for bfs
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(root);
+        //going through the nodes in the queue
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i=0; i< size; i++){
+                Node curr = queue.poll();
+                //if the node is not the last node in its level
+                if(i<size-1) curr.next = queue.peek();
+                //add the left and right child to the queue
+                if(curr.left != null) queue.add(curr.left);
+                if(curr.right != null) queue.add(curr.right);
+            }
+        }
+        return root;
+    }
+}
+
+//Time and space complexity of O(n) where n is the number of nodes in the tree, 
+//we need to check all the nodes at least once and we store them in a queue of size of n. 
